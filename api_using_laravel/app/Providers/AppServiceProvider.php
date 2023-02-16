659875f51bef3a9c73ba3ee2026f9e72ba77ec2a
Mohammed Ammar Ammar <m.ammar4web@gmail.com>
2023-02-16 18:40:47 +0300

    Manage User's Vehicles

--- api_using_laravel/app/Providers/AppServiceProvider.php
+++ api_using_laravel/app/Providers/AppServiceProvider.php
@@ -2,6 +2,10 @@
 
 namespace App\Providers;
 
+//
+use App\Models\Vehicle;
+use App\Observers\VehicleObserver;
+//
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +27,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        //
+        Vehicle::observe(VehicleObserver::class);
         //
     }
 }
--- api_using_laravel/routes/api.php
+++ api_using_laravel/routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
 use \App\Http\Controllers\Api\V1\Auth;
-
+use App\Http\Controllers\Api\V1\VehicleController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -28,4 +28,6 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::put('profile', [Auth\ProfileController::class, 'update']);
     Route::put('password', Auth\PasswordUpdateController::class);
     Route::post('auth/logout', Auth\LogoutController::class);
+    //
+    Route::apiResource('vehicles', VehicleController::class);
 });
