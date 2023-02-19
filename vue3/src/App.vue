4c4acc0280e50c4765dce018e53043e8ac826f50
Mohammed Ammar Ammar <m.ammar4web@gmail.com>
2023-02-19 12:13:53 +0300

    Authentication

--- vue3/src/App.vue
+++ vue3/src/App.vue
@@ -1,10 +1,13 @@
 <script setup>
 import { RouterLink, RouterView } from "vue-router";
+import { useAuth } from "@/stores/auth";
+
+const auth = useAuth();
 </script>
 
 <template>
   <header class="py-6 bg-gray-100 shadow">
-    <div class="container md:px-2 px-4 mx-auto">
+    <div class="container lg:px-20 md:px-2 px-4 mx-auto">
       <nav class="flex gap-4 justify-between">
         <div class="flex gap-4 items-center">
           <h2 class="text-xl font-bold">
@@ -15,20 +18,33 @@ import { RouterLink, RouterView } from "vue-router";
             </div>
             myParking
           </h2>
-          <RouterLink class="router-link" :to="{ name: 'home' }">
-            Home
-          </RouterLink>
+
+          <template v-if="auth.check">
+            <RouterLink class="router-link" :to="{ name: 'vehicles.index' }">
+              Vehicles
+            </RouterLink>
+          </template>
+          <template v-else>
+            <RouterLink class="router-link" :to="{ name: 'home' }">
+              Home
+            </RouterLink>
+          </template>
         </div>
         <div class="flex gap-4 items-center">
-          <RouterLink class="router-link" :to="{ name: 'register' }">
-            Register
-          </RouterLink>
+          <template v-if="auth.check">
+            <button @click="auth.logout" class="router-link">Logout</button>
+          </template>
+          <template v-else>
+            <RouterLink class="router-link" :to="{ name: 'register' }">
+              Register
+            </RouterLink>
+          </template>
         </div>
       </nav>
     </div>
   </header>
 
-  <div class="container md:px-2 px-4 pt-8 md:pt-16 mx-auto">
+  <div class="container lg:px-20 md:px-2 px-4 pt-8 md:pt-16 mx-auto">
     <RouterView />
   </div>
 </template>
