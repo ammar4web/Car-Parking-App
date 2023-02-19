9ae5bb40c4bc8c78df2bdc42e2fddcf8f43fc696
Mohammed Ammar Ammar <m.ammar4web@gmail.com>
2023-02-19 16:38:51 +0300

    tailwindcss/colors

--- react-vite/tailwind.config.cjs
+++ react-vite/tailwind.config.cjs
@@ -1,11 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: { colors },
   },
   plugins: [],
 }
