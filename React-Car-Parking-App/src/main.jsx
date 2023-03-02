efe1cfd0fe0795f1796cf84161419b8e704d7bc8
Mohammed Ammar Ammar <m.ammar4web@gmail.com>
2023-03-02 13:38:05 +0300

    fix baseURL with Axios library

--- React-Car-Parking-App/src/main.jsx
+++ React-Car-Parking-App/src/main.jsx
@@ -12,7 +12,7 @@ import { route } from '@/routes'
 window.axios = axios
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
 window.axios.defaults.withCredentials = true
-window.axios.defaults.baseURL = 'http://parkingapi.test/api/v1'
+window.axios.defaults.baseURL = 'http://127.0.0.1:8100/api/v1'
  
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
