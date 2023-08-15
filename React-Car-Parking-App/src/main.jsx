89de87db8f40c8ccb7b80b1f89be2dace03c0425
Mohammed Ammar Ammar <m.ammar4web@gmail.com>
2023-08-15 18:56:44 +0300

    Authentication

--- React-Car-Parking-App/src/main.jsx
+++ React-Car-Parking-App/src/main.jsx
@@ -5,6 +5,7 @@ import axios from 'axios'
 import App from '@/App'
 import Home from '@/views/Home'
 import Register from '@/views/auth/Register'
+import Login from '@/views/auth/Login'
 import VehiclesList from '@/views/vehicles/VehiclesList'
 import '@/assets/main.css'
 import { route } from '@/routes'
@@ -12,7 +13,7 @@ import { route } from '@/routes'
 window.axios = axios
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
 window.axios.defaults.withCredentials = true
-window.axios.defaults.baseURL = 'http://127.0.0.1:8100/api/v1'
+window.axios.defaults.baseURL = 'http://parkingapi.test/api/v1'
  
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -21,6 +22,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <Route path={ route('home') } element={<App />}>
           <Route index element={<Home />} />
           <Route path={ route('register') } element={<Register />} />
+          <Route path={ route('login') } element={<Login />} />
           <Route path={ route('vehicles.index') } element={<VehiclesList />} />
         </Route>
       </Routes>
